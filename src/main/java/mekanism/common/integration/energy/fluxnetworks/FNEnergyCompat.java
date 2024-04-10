9814550b64c56f00e4516d6d8cdab9aff3465874
Lapis <45414333+Lapis256@users.noreply.github.com>
2024-04-10 20:21:25 +0900

    Backporting Flux Networks integration from 1.19.x

--- src/main/java/mekanism/common/integration/energy/fluxnetworks/FNEnergyCompat.java
+++ src/main/java/mekanism/common/integration/energy/fluxnetworks/FNEnergyCompat.java
@@ -1,37 +1,34 @@
-/*package mekanism.common.integration.energy.fluxnetworks;
+package mekanism.common.integration.energy.fluxnetworks;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.annotation.ParametersAreNonnullByDefault;
+import mekanism.api.annotations.NothingNullByDefault;
 import mekanism.api.energy.IStrictEnergyHandler;
 import mekanism.common.Mekanism;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.integration.energy.IEnergyCompat;
 import mekanism.common.util.CapabilityUtils;
+import mekanism.common.util.UnitDisplayUtils.EnergyUnit;
 import net.minecraft.core.Direction;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.Nullable;
 import sonar.fluxnetworks.api.energy.IFNEnergyStorage;
 
-@ParametersAreNonnullByDefault
+@NothingNullByDefault
 public class FNEnergyCompat implements IEnergyCompat {
 
-    private static final Capability<IFNEnergyStorage> FN_ENERGY_CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
+    private static final Capability<IFNEnergyStorage> FN_ENERGY_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {});
 
-    @Nonnull
     @Override
     public Capability<?> getCapability() {
         return FN_ENERGY_CAPABILITY;
     }
 
     @Override
-    public boolean isMatchingCapability(@Nonnull Capability<?> capability) {
-        if (Mekanism.hooks.FluxNetworksLoaded) {
-            //Ensure we check that Flux networks is loaded before attempting to access their capability
-            return capability == FN_ENERGY_CAPABILITY;
-        }
-        return false;
+    public boolean isMatchingCapability(Capability<?> capability) {
+        return capability == FN_ENERGY_CAPABILITY;
     }
 
     @Override
@@ -39,15 +36,13 @@ public class FNEnergyCompat implements IEnergyCompat {
         return EnergyUnit.FORGE_ENERGY.isEnabled() && Mekanism.hooks.FluxNetworksLoaded && !MekanismConfig.general.blacklistFluxNetworks.get();
     }
 
-    @Nonnull
     @Override
-    public LazyOptional<?> getHandlerAs(@Nonnull IStrictEnergyHandler handler) {
+    public LazyOptional<?> getHandlerAs(IStrictEnergyHandler handler) {
         return LazyOptional.of(() -> new FNIntegration(handler));
     }
 
-    @Nonnull
     @Override
     public LazyOptional<IStrictEnergyHandler> getLazyStrictEnergyHandler(ICapabilityProvider provider, @Nullable Direction side) {
         return CapabilityUtils.getCapability(provider, FN_ENERGY_CAPABILITY, side).lazyMap(FNStrictEnergyHandler::new);
     }
-}*/
+}
